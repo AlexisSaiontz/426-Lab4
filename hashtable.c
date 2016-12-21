9cee926ecef4e5a01d175584b8532713929a9f15
steliosrousoglou <stylianos.rousoglou@yale.edu>
2016-12-20 19:26:37 -0500

    Update hashtable.c

--- hashtable.c
+++ hashtable.c
@@ -67,83 +67,21 @@ bool add_vertex(uint64_t id) {
 	return true;
 }
 
-// helper code 
-void fix_edges(vertex *out){
-	edge *head = out->head;
-	uint64_t id = out->id;
-	while(head){
-		LL_delete(&((ret_vertex(head->b))->head), id);
-		head=head->next;
-	}
-}
-
-
-// Helper, returns false if vertex does not exist
-bool delete_vertex(vertex** head, uint64_t id)
-{
-    if(*head == NULL) return false;
-    else {
-        vertex* tmp = *head;
-        if(tmp->id == id) {
-            fix_edges(tmp);
-            tmp = tmp->next;
-            free(*head);
-            *head = tmp;
-            return true;
-        }
-
-        while(tmp->next)
-        {
-            if(tmp->next->id == id) {
-                vertex* tmp2 = tmp->next->next;
-                fix_edges(tmp->next);
-                free(tmp->next);
-                tmp->next = tmp2;
-                return true;
-            }
-            tmp = tmp->next;
-        }
-    }
-    return false;
-}
-
-// Removes vertex, returns false is vertex does not exist
-bool remove_vertex(uint64_t id) {
-	int hash = hash_vertex(id);
-	vertex** table = map.table;
-
-	if(delete_vertex(&(table[hash]), id)) {
-		map.nsize -= 1;
-		return true;
-	} else return false;
-}
-
 // Check if a vertex is in a graph. 
-bool get_node(uint64_t id){
-	
-
-
+bool get_node(uint64_t id) {
 	if (ret_vertex(id) == NULL){
 		return false;
 	}
-
-
-
 	return true;
 }
 
 // Check if an edge is in a graph 
 bool get_edge(uint64_t a, uint64_t b){
-
-
-
 	vertex *v1 = ret_vertex(a);
 	vertex *v2 = ret_vertex(b);
 	if (LL_contains(&(v1->head), b) && LL_contains(&(v2->head), a)){
-		
 		return true;
 	}
-
 	return false;
 }
 
@@ -210,8 +148,6 @@ bool LL_delete(edge** head, uint64_t n)
 // Adds edge, returns 
 int add_edge(uint64_t a, uint64_t b) {
 
-
-
 	vertex** table = map.table;
 
 	vertex* v1 = table[hash_vertex(a)];
@@ -227,13 +163,11 @@ int add_edge(uint64_t a, uint64_t b) {
 		if (v2->id == b) break;
 		v2 = v2->next;
 	}
-
 	// code 400
 	if(!v1 || !v2 || a == b){
 
 		 return 400;
 	}
-
 	if(LL_contains(&(v1->head), b)) {
 
 		return 204;
@@ -241,16 +175,11 @@ int add_edge(uint64_t a, uint64_t b) {
 	LL_insert(&(v1->head), b);
 	LL_insert(&(v2->head), a);
 	map.esize += 1;
-	
-
 	return 200;
 }
 
 // Removes edge, returns false if it didn't exist
 bool remove_edge(uint64_t a, uint64_t b) {
-
-
-
 	vertex** table = map.table;
 	
 	vertex* v1 = table[hash_vertex(a)];
@@ -273,9 +202,7 @@ bool remove_edge(uint64_t a, uint64_t b) {
 		return false;
 	}
 	map.esize -= 1;
-	bool r = LL_delete(&(v1->head), b) && LL_delete(&(v2->head), a);
-
-	return r;
+	return LL_delete(&(v1->head), b) && LL_delete(&(v2->head), a);
 }
 
 /*
@@ -334,48 +261,6 @@ void queue_destroy(queue **q){
 /*
 	Other operations
 */
-
-// Assumes both nodes exist; returns value of shortest path
-int shortest_path(uint64_t id1, uint64_t id2){
-	
-	queue *resetqueue = queueCreate();
-	queue *bfsQ = queueCreate();
-	vertex *root=ret_vertex(id1);
-
-	root->path=0;
-	
-	vertex *current;
-	vertex *n;
-	edge *runner;
-
-	enqueue(&bfsQ, root->id);
-	enqueue(&resetqueue, root->id);
-	int shortest_path = -1;
-	while (bfsQ->head != 0){
-		current=ret_vertex(dequeue(&bfsQ));
-		for (runner = current->head; 
-			runner != NULL; 
-			runner = runner->next){
-			n = ret_vertex(runner->b);
-			if (n->path == -1){
-				n->path = current->path + 1;
-				enqueue(&resetqueue, n->id);
-				enqueue(&bfsQ, n->id);
-			}
-		}
-	}
-	shortest_path=(ret_vertex(id2))->path;
-	vertex *reset;
-	while(resetqueue->head !=0){
-		reset = ret_vertex(dequeue(&resetqueue));
-		reset->path = -1;
-	}
-	queue_destroy(&resetqueue);
-	queue_destroy(&bfsQ);
-	return shortest_path;
-
-}
-
 // Given a valid node_id, returns list of neighbors
 uint64_t *get_neighbors(uint64_t id, int* n){
 	vertex *v = ret_vertex(id);
